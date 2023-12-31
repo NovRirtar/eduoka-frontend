4ad1e7bec1ea2307860903deec4ccea34741cafd
NovRirtar <rirtar.lofi@gmail.com>
2023-12-31 20:52:58 +0700

    update code

--- js/index.js
+++ js/index.js
@@ -25,3 +25,31 @@ $(document).ready(function () {
   $(document).ready(function () {
     $("#select2").select2();
   });
+
+  //
+  // $(document).ready(function() {
+  //   var currentSlide = 0;
+  //   var totalSlides = Math.ceil($(".col-md-4").length / 3);
+
+  //   function showSlide(index) {
+  //     $(".col-md-4").addClass("hide");
+  //     var startIndex = index * 3;
+  //     var endIndex = startIndex + 3;
+  //     $(".col-md-4").slice(startIndex, endIndex).removeClass("hide");
+  //     $(".slider-container").css("transform", "translateX(" + -index * 33.333333 + "%)");
+  //   }
+
+  //   function nextSlide() {
+  //     currentSlide = (currentSlide + 1) % totalSlides;
+  //     showSlide(currentSlide);
+  //   }
+
+  //   setInterval(nextSlide, 1000); // Tự động chuyển slide sau 3 giây
+
+  //   // Bắt sự kiện khi click nút chuyển slide
+  //   $(".next-btn").click(function() {
+  //     nextSlide();
+  //   });
+  // });
+
+
